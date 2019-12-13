4f0f5ae7d771c1e006bdfc62f317e317d96305bd
Collin Jilbert <cjilbert504@gmail.com>
2019-12-13 14:25:43 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Project
+
+  attr_reader :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    self.backers << backer
+  end
+
+end
